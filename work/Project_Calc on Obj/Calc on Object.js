efada42e7579332f589de73828e4ec39300954fe
YaRolly <Ea4GoodRu@gmail.com>
2022-01-29 17:39:20 +0300

    Create Calc on Object.js

---
+++ work/Project_Calc on Obj/Calc on Object.js
@@ -0,0 +1,40 @@
+
+
+const list = {
+  "create a new practice task": "In Progress",
+  "make a bed": "Done",
+  "write a post": "To Do",
+}
+
+function changeStatus(task,status) {
+  list[task] = status;
+};
+
+function addTask(task) {
+  list[task] = "To Do";
+};
+
+function deleteTask(task) {
+  delete list[task];
+};
+
+function showList() {
+  let taskToDo = '';
+  let taskInProgress = '';
+  let taskDone = ''
+  for (let task in list) {
+    switch (list[task]) {
+      case "To Do" :
+        taskToDo += ` ${task}\n`;
+        break;
+      case "In Progress" :
+        taskInProgress += ` ${task}\n`;
+        break;
+      case "Done" :
+        taskDone += ` ${task}\n`;
+        break;
+    }
+  }
+  return `Todo:\n${taskToDo || '-'} \nIn Progress :\n${taskInProgress || '-'} \nDone:\n${taskDone || '-'} `;
+};
+console.log(showList());
